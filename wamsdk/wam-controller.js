f5bb2e7b1745621fa0b857020f5767cdc5b694f5
jariseon <jari.kleimola@outlook.com>
2018-10-04 19:08:56 +0300

    added get/setState

--- wamsdk/wam-controller.js
+++ wamsdk/wam-controller.js
@@ -6,7 +6,7 @@ class WAMController extends AudioWorkletNode
 {
   constructor(context, processorName, options) {
     super(context, processorName, options);
-    
+
     var self = this;
     this.port.onmessage = function (e) {
       var msg = e.data;
@@ -14,28 +14,28 @@ class WAMController extends AudioWorkletNode
       catch(e) { }
       if (msg) {
         if (msg.type == "descriptor")
-          self.descriptor = msg.data;      
-        self.onmessage(msg);        
+          self.descriptor = msg.data;
+        self.onmessage(msg);
       }
     }
   }
-  
+
   setParam(key,value) {
-    this.port.postMessage({ type:"param", key:key, value:value });    
+    this.port.postMessage({ type:"param", key:key, value:value });
   }
 
   setPatch(patch) {
-    this.port.postMessage({ type:"patch", data:patch });    
+    this.port.postMessage({ type:"patch", data:patch });
   }
 
   setSysex(sysex) {
-    this.port.postMessage({ type:"sysex", data:sysex });    
+    this.port.postMessage({ type:"sysex", data:sysex });
   }
 
   onMidi(msg) {
     this.port.postMessage({ type:"midi", data:msg });
   }
-  
+
   set midiIn (port) {
     if (this._midiInPort) {
       this._midiInPort.close();
@@ -46,12 +46,20 @@ class WAMController extends AudioWorkletNode
       this.port.postMessage({ type:"midi", data:msg.data });
     }.bind(this);
   }
-  
+
   sendMessage(verb, prop, data) {
     this.port.postMessage({ type:"msg", verb:verb, prop:prop, data:data });
   }
-  
+
   get gui () { return null; }
 
   onmessage (msg) { }
+
+  getState () {
+    return Promise.resolve(null);
+  }
+
+  setState (blob) {
+    return Promise.resolve();
+  }
 }
