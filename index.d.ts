3f418cbdd9ddad129dbb66f52b5a2744a5234373
Fr0stbyteR <renshihong@hotmail.com>
2021-01-14 16:41:07 +0800

    add one point export for the API

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export { default as AbstractWamNode } from './AbstractWamNode.js';
+export { default as AbstractWamParameter } from './AbstractWamParameter.js';
+export { default as AbstractWamParameterInfo } from './AbstractWamParameterInfo.js';
+export { default as AbstractWamProcessor } from './AbstractWamProcessor.js';
+export { default as AbstractWebAudioModule } from './AbstractWebAudioModule.js';
+export * from './types';
---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as AbstractWamNode } from './AbstractWamNode.js';
+export { default as AbstractWamParameter } from './AbstractWamParameter.js';
+export { default as AbstractWamParameterInfo } from './AbstractWamParameterInfo.js';
+export { default as AbstractWamProcessor } from './AbstractWamProcessor.js';
+export { default as AbstractWebAudioModule } from './AbstractWebAudioModule.js';
